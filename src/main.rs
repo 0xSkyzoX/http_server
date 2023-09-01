b5056506adb1e25a342cd4500340a2d633fc53da
Eddarif Badr <skyzoxcodes@gmail.com>
2023-09-01 12:03:21 +0100

    Hello, world!

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ mod http;
 
 fn main() {
     let mut server = http::HttpServer::new("0.0.0.0:5000");
-    server.add_route("/", "Hello My Friend");
+    server.add_route("/", "Hello, world!");
     server.start();
 }
